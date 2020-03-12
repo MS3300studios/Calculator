086df7c82a5a6ebc7d31c862f737ffca20d94013
Mikołaj Strusiński <mikistrus@gmail.com>
2020-03-12 12:09:18 +0100

    Added basic app functionality
    The Calculator can now perform actions o subtraction, addition, division and multiplication.
    It also has the "clear" button, which clears the textboxes.

--- CalculatorSolution/Calculator/Form1.Designer.cs
+++ CalculatorSolution/Calculator/Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.LabelResult = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttondivide = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(26, 28);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(51, 20);
+            this.textBoxA.TabIndex = 2;
+            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // LabelResult
+            // 
+            this.LabelResult.AutoSize = true;
+            this.LabelResult.Location = new System.Drawing.Point(12, 134);
+            this.LabelResult.Name = "LabelResult";
+            this.LabelResult.Size = new System.Drawing.Size(54, 13);
+            this.LabelResult.TabIndex = 6;
+            this.LabelResult.Text = "labelresult";
+            this.LabelResult.Visible = false;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(99, 28);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(48, 20);
+            this.textBoxB.TabIndex = 7;
+            this.textBoxB.TextChanged += new System.EventHandler(this.textBoxB_TextChanged);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(26, 65);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(39, 22);
+            this.buttonPlus.TabIndex = 8;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(155, 205);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(71, 64);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(42, 24);
+            this.buttonMinus.TabIndex = 10;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 24);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "*";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttondivide
+            // 
+            this.buttondivide.Location = new System.Drawing.Point(167, 64);
+            this.buttondivide.Name = "buttondivide";
+            this.buttondivide.Size = new System.Drawing.Size(44, 24);
+            this.buttondivide.TabIndex = 12;
+            this.buttondivide.Text = "/";
+            this.buttondivide.UseVisualStyleBackColor = true;
+            this.buttondivide.Click += new System.EventHandler(this.buttondivide_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(363, 240);
+            this.Controls.Add(this.buttondivide);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.LabelResult);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label LabelResult;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttondivide;
     }
 }
 
--- CalculatorSolution/Calculator/Form1.cs
+++ CalculatorSolution/Calculator/Form1.cs
@@ -16,5 +16,64 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void textBoxA_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+        //addition
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBoxA.Text);
+            int b = int.Parse(textBoxB.Text);
+            LabelResult.Visible = true;
+            LabelResult.Text = $"{a+b}";
+        }
+
+        private void textBoxB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        
+        //multiplication
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBoxA.Text);
+            int b = int.Parse(textBoxB.Text);
+            LabelResult.Visible = true;
+            LabelResult.Text = $"{a * b}";
+
+        }
+        //subtraction
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBoxA.Text);
+            int b = int.Parse(textBoxB.Text);
+            LabelResult.Visible = true;
+            LabelResult.Text = $"{a - b}";            
+        }
+        //division
+        private void buttondivide_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBoxA.Text);
+            int b = int.Parse(textBoxB.Text);
+            if (b <= 0)
+            {
+                LabelResult.Visible = true;
+                LabelResult.Text = "You cannot divide by 0! Press Clear and try again.";                
+            }
+            else
+            {
+                LabelResult.Visible = true;
+                LabelResult.Text = $"{a / b}";                
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxA.Clear();
+            textBoxB.Clear();
+        }
     }
 }

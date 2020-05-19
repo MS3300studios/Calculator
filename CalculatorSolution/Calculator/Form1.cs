7b5502218f4572f6540835116907ee00c2959942
Mikołaj Strusiński <mikistrus@gmail.com>
2020-05-19 10:33:49 +0200

    history functionality for subtraction

--- CalculatorSolution/Calculator/Form1.cs
+++ CalculatorSolution/Calculator/Form1.cs
@@ -87,7 +87,10 @@ namespace Calculator
             double a = Convert.ToDouble(textBoxA.Text);
             double b = Convert.ToDouble(textBoxB.Text);
             LabelResult.Visible = true;
-            LabelResult.Text = $"{a - b}";
+            double abresult = a - b;
+            LabelResult.Visible = true;
+            LabelResult.Text = $"{abresult}";
+            history.Add($"{history.Count + 1}.     {a}-{b}={abresult}");
 
         }
         //division
